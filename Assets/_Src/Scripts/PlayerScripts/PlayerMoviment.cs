21d243e0c0b85dabb2296ea034571c6c5fc163d7
thiagoed-cntdev <86066295+thiagoed-cntdev@users.noreply.github.com>
2024-09-16 19:34:00 -0300

    Movimento boneco

---
+++ Assets/_Src/Scripts/PlayerScripts/PlayerMoviment.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour // é um script que age dentro do jogo
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
